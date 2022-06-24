3413f8d4572a921faf507c1c3b33e7ed816711d6
EhWhoAmI <action@github.com>
2022-06-24 13:47:05 +0000

    Update from https://github.com/Conquer-Space/Conquer-Space/commit/8441dbd8262429bedce2d6c931f745877d281066

--- docs/search/classes_a.js
+++ docs/search/classes_a.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['lab_0',['Lab',['../structcqsp_1_1common_1_1components_1_1science_1_1_lab.html',1,'cqsp::common::components::science']]],
-  ['layerrenderer_1',['LayerRenderer',['../classcqsp_1_1engine_1_1_layer_renderer.html',1,'cqsp::engine']]],
-  ['lightemitter_2',['LightEmitter',['../structcqsp_1_1common_1_1components_1_1bodies_1_1_light_emitter.html',1,'cqsp::common::components::bodies']]],
-  ['loadingdatamodel_3',['LoadingDataModel',['../structcqsp_1_1scene_1_1_loading_scene_1_1_loading_data_model.html',1,'cqsp::scene::LoadingScene']]],
-  ['loadingscene_4',['LoadingScene',['../classcqsp_1_1scene_1_1_loading_scene.html',1,'cqsp::scene']]]
+  ['launchvehicle_1',['LaunchVehicle',['../structcqsp_1_1common_1_1components_1_1_launch_vehicle.html',1,'cqsp::common::components']]],
+  ['layerrenderer_2',['LayerRenderer',['../classcqsp_1_1engine_1_1_layer_renderer.html',1,'cqsp::engine']]],
+  ['lightemitter_3',['LightEmitter',['../structcqsp_1_1common_1_1components_1_1bodies_1_1_light_emitter.html',1,'cqsp::common::components::bodies']]],
+  ['loadingdatamodel_4',['LoadingDataModel',['../structcqsp_1_1scene_1_1_loading_scene_1_1_loading_data_model.html',1,'cqsp::scene::LoadingScene']]],
+  ['loadingscene_5',['LoadingScene',['../classcqsp_1_1scene_1_1_loading_scene.html',1,'cqsp::scene']]]
 ];
